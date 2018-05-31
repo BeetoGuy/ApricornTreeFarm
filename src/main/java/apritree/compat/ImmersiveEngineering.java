03dc481b2f7b5423db2f92c95bfa743fb11f583a
BeetoGuy <BeetoGuy@gmail.com>
2018-05-31 01:50:28 -0700

    Make mechanical anvil compatible with IE heater

--- src/main/java/apritree/compat/ImmersiveEngineering.java
+++ src/main/java/apritree/compat/ImmersiveEngineering.java
@@ -5,7 +5,9 @@ import apritree.utils.AdvAnvilCrafting;
 import apritree.utils.AnvilRegistry;
 import blusunrize.immersiveengineering.api.crafting.BlueprintCraftingRecipe;
 import blusunrize.immersiveengineering.api.crafting.MetalPressRecipe;
+import blusunrize.immersiveengineering.api.tool.ExternalHeaterHandler;
 import blusunrize.immersiveengineering.common.IEContent;
+import com.pixelmonmod.pixelmon.blocks.tileEntities.TileEntityMechanicalAnvil;
 import net.minecraft.item.ItemStack;
 
 public class ImmersiveEngineering {
@@ -14,5 +16,7 @@ public class ImmersiveEngineering {
         for (AdvAnvilCrafting craft : AnvilRegistry.getInstance().getCraftingRecipes()) {
             MetalPressRecipe.addRecipe(craft.getOutput(), craft.getInput(), new ItemStack(ApriRegistry.ball_mold), 3200);
         }
+
+        ExternalHeaterHandler.registerHeatableAdapter(TileEntityMechanicalAnvil.class, new AnvilHeatingWrapper());
     }
 }
